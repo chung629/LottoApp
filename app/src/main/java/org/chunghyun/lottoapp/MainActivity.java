55c999f72b56b3a35b71d07af7934782436b0db7
chunghyun <chung629@naver.com>
2021-01-18 20:05:54 +0900

    xml finish

--- app/src/main/java/org/chunghyun/lottoapp/MainActivity.java
+++ app/src/main/java/org/chunghyun/lottoapp/MainActivity.java
@@ -3,12 +3,49 @@ package org.chunghyun.lottoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, getString(R.string.admob_app_id));
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        // 광고가 제대로 로드 되는지 테스트 하기 위한 코드입니다.
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                // 광고가 문제 없이 로드시 출력됩니다.
+                Log.d("@@@", "onAdLoaded");
+            }
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+                // 광고 로드에 문제가 있을시 출력됩니다.
+                Log.d("@@@", "onAdFailedToLoad " + errorCode);
+            }
+            @Override
+            public void onAdOpened() { }
+            @Override
+            public void onAdClicked() { }
+            @Override
+            public void onAdLeftApplication() { }
+            @Override
+            public void onAdClosed() { }
+        });
     }
 }
